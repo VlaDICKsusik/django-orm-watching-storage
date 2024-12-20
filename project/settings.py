f8bfca823bb0a6953d0e96bced5c29b3929c869e
VlaDICKsusik <vladgorbackii@gmail.com>
2024-12-20 18:39:13 +0300

    changes

--- project/settings.py
+++ project/settings.py
@@ -1,29 +1,27 @@
 import os
-from logging import DEBUG
 
-from environs import Env
+#os.environ.get('HOME', '/var/www')
 
-env = Env()
-env.read_env()
-
+#secretss
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": os.environ["DB_HOST"],
-        "PORT": os.environ["DB_PORT"],
-        "NAME": os.environ["DB_NAME"],
-        "USER":  os.environ["DB_USER"],
-        "PASSWORD":  os.environ["DB_PASSWORD"],
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER":  os.environ.get("DB_USER"),
+        "PASSWORD":  os.environ.get("DB_PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ["datacenter"]
 
-SECRET_KEY = os.environ["SECRET_KEY"]
-DEBUG =  os.environ["DEBUG"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG =  os.environ.get("DEBUG")
 
 ROOT_URLCONF = "project.urls"
-ALLOWED_HOSTS= os.environ["ALLOWED_HOSTS"]
+
+ALLOWED_HOSTS= os.environ.get("ALLOWED_HOSTS")
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
